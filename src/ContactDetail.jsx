7553a96d0705b9ec5791f6a4a49a7fb0c3bdde33
unknown <arifrakhmatn@gmail.com>
2021-01-26 19:17:55 +0700

    styling dan fix data

--- src/ContactDetail.jsx
+++ src/ContactDetail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Close from '@material-ui/icons/Close';
 
 export default function ContactModal(props) {
   const { open, onClose, item = {}, onDelete } = props;
@@ -13,28 +14,43 @@ export default function ContactModal(props) {
     >
     {open &&
       <div>
+        <div className="dialog-header">
+          <span>Kontak Detail</span>
+          <Close onClick={onClose} />
+        </div>
         <img src={item.picture.large} style={{ width: '100%', height: '100%' }} />
         <div style={{ padding: 10 }}>
           <span>{item.name.first} {item.name.last}</span>
           <div className="info-container">
-            <div><span className="info-text">Lahir</span><span>{item.dob.date}</span></div>
-            <div><span className="info-text">Jenis kelamin</span><span>{item.gender === 'male' ? 'Laki-laki' : 'Perempuan'}</span></div>
+            <div className="info-warpper">
+              <span className="info-text">Lahir</span><span>{item.dob.date}</span>
+            </div>
+            <div className="info-warpper">
+              <span className="info-text">Jenis kelamin</span>
+              <span>{item.gender === 'male' ? 'Laki-laki' : 'Perempuan'}</span>
+            </div>
           </div>
           <div className="info-container">
-            <div><span className="info-text">Surel</span><span>{item.email}</span></div>
-            <div><span className="info-text">Telepon</span><span>{item.phone}</span></div>
-            <div><span className="info-text">Seluler</span><span>{item.cell}</span></div>
+            <div className="info-warpper">
+              <span className="info-text">Surel</span><span>{item.email}</span>
+            </div>
+            <div className="info-warpper">
+              <span className="info-text">Telepon</span><span>{item.phone}</span>
+            </div>
+            <div className="info-warpper">
+              <span className="info-text">Seluler</span><span>{item.cell}</span>
+            </div>
           </div>
           <div className="info-container">
-            <div>
+            <div className="info-warpper">
               <span className="info-text">Tempat Tinggal</span>
               <span>{item.location.street.number} {item.location.street.name} {item.location.city} {item.location.state} {item.location.postcode}</span>
             </div>
             <div>
               <span className="info-text">Koordinat</span>
-              <span>{item.location.coordinates.latitude} {item.location.coordinates.langitude}</span></div>
+              <span>{item.location.coordinates.latitude}, {item.location.coordinates.longitude}</span></div>
           </div>
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <div style={{ display: 'flex', justifyContent: 'flex-end', borderTop: 'solid 1px #bbbbbb' }}>
             <Button
               variant="contained"
               onClick={onDelete}
